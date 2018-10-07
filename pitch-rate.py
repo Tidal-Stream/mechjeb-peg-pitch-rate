0960d8513262eabdfb5053f01bf7cc7822a6260f
Tidal Stream <tidalstream@yandex.ru>
2018-10-07 23:58:47 +0300

    Add pitch and non-constant gravity

--- pitch-rate.py
+++ pitch-rate.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
-g = 9.81                   # grav acc in m/s^2
+import math
+
+g = 9.81                        # grav acc in meters/second^2
+r0 = 6_378_100                  # Earth radius in meters
+muggc = 3.986004418e+14         # geocentric gravitational constant
 
 def exhaustVelocityFromISP(isp: float) -> float:
     return isp * g
 
-def simStage(stageTime: int, thrust: float, massEmpty: float, massPropellant: float, massFull: float, isp: float) -> int:
-    x = 0                      # altitude in meters
-    v = 0                      # velocity in meters/sec
-    t = 0                      # time in sec
+def simStage(x: float, y: float, vx: float, vy: float, stageTime: int, thrust: float, massEmpty: float, massPropellant: float, massFull: float, isp: float, pitchRateDeg: float) -> (float, float, float, float):
+    t = 0                       # time in sec
     dt = 1
     mass = massFull
-    velocityE = isp * g        # tsfc = 1 / velocityE
-    dm = - (thrust / velocityE * dt)
+    velocityE = isp * g         # tsfc = 1 / velocityE
+    dm = -(thrust / velocityE * dt)
+    pitchRate = math.radians(pitchRateDeg)
+    pitch = pitchRate           # for now use a fixed starting pitch
 
     # loop with some small dt, thrust is constant thrust (in Newtons -- not
     # kN), g is 9.8 m/s, mdot is based on thrust and ISP (should be in kg/sec)
@@ -20,22 +24,38 @@ def simStage(stageTime: int, thrust: float, massEmpty: float, massPropellant: fl
     # planet for g):
 
     while t <= stageTime:
-        dx = v * dt
-        dv = (thrust/mass - g) * dt
+        rm3 = math.sqrt(x ** 2 + y ** 2) ** 3
+
+        uy = math.sin(pitch)            # velocity in meters/sec
+        ux = math.cos(pitch)
+
+        dx = vx * dt
+        dy = vy * dt
+        dvx = (-muggc * x / rm3 + thrust / mass * ux) * dt;
+        dvy = (-muggc * y / rm3 + thrust / mass * uy) * dt;
+
         x += dx
-        v += dv
+        y += dy
+        vx += dvx
+        vy += dvy
         mass += dm
         t += dt
+        pitch = math.atan2(vy, vx)
 
-    print('x = {0:.0f}'.format(x))
-    print('v = {0:.1f}'.format(v))
+    print('x = {0:.0f} m'.format(x))
+    print('x - r0 = {0:.0f} m'.format(x - r0))
+    print('y = {0:.0f} m'.format(y))
+    print('vx = {0:.1f} m/s'.format(vx))
+    print('vy = {0:.1f} m/s'.format(vy))
+    print('pitch = {0:.1f} deg'.format(math.degrees(pitch)))
+    return (x, y, vx, vy)
 
 
 def main():
     stage1Empty = 21_054 + 2_316 + 20_830
     stage1Propellant = 284_089
     stage1Full = stage1Empty + stage1Propellant
-    simStage(253, 3_827_000, stage1Empty, stage1Propellant, stage1Full, 311.3)
+    simStage(r0, 0, 0, 0, 253, 3_827_000, stage1Empty, stage1Propellant, stage1Full, 311.3, 0.0000062)
 
 
 if __name__ == '__main__':
